46842c3445ff6f7059bf1a93777aa757510a7619
keyu.wang <me@keyu.email>
2022-06-20 01:32:04 +0800

    feat: CreateUsers

--- src/seed.ts
+++ src/seed.ts
@@ -1,13 +1,6 @@
 import 'reflect-metadata';
 import {AppDataSource} from './data-source';
-import {Post} from './entity/Post';
 
 AppDataSource.initialize().then(async (connection) => {
-  const posts = await connection.manager.find(Post);
-  if (posts.length === 0) {
-    await connection.manager.save([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(n => {
-      return new Post({title: `Post ${n}`, content: `第${n}篇文章`});
-    }));
-  }
-  await connection.destroy();
+  await connection.destroy()
 }).catch(error => console.log(error));
