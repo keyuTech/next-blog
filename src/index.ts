0fb2377482322c1f72e1de5403de8505fa25d09d
keyu.wang <me@keyu.email>
2022-06-20 00:16:40 +0800

    feat: add typeorm

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import 'reflect-metadata'
-import { AppDataSource } from "./data-source"
-
-AppDataSource.initialize().then(async () => {
-    console.log('typeorm');
-}).catch(error => console.log(error))
