42da5266e9de0bea200adb579cc07b1591b0834d
keyu <me@keyu.email>
2022-07-15 02:28:37 +0800

    feat: update seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,15 +2,38 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 const main = async () => {
-  const kkk = await prisma.user.upsert({
+  const user1 = await prisma.user.upsert({
     where: {id: 1},
     create: {
-      username: 'kkkk',
+      username: 'user1',
       password_digest: 'aaa'
     },
     update: {}
   });
   console.log('create user');
+  const post1 = await prisma.post.upsert({
+    where: {id: 1},
+    create: {
+      title: 'First post',
+      content: 'My first post',
+      author: {
+        connect: {id: 1}
+      },
+      comments: {}
+    },
+    update: {}
+  })
+  const comment1 = await prisma.comment.create({
+    data: {
+      content: 'comment 1',
+      author: {
+        connect: {id: 1}
+      },
+      post: {
+        connect: {id: 1}
+      }
+    }
+  })
 };
 
 main().catch((e) => {
