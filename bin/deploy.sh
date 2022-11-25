639ced079fcd4431245335b2f1040f44b3a5d055
keyu <me@keyu.email>
2022-11-25 23:26:33 +0800

    feat: update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,7 +7,16 @@ yarn install --production=false &&
 npx prisma generate &&
 yarn build && 
 docker build -t keyu/blog-app . &&
-docker kill app &&
-docker rm app &&
-docker run --name app --network=host -p 3000:3000 -d keyu/blog-app &&
+exist=`docker inspect --format '{{.State.Running}}' blog-app`
+if [ "${exist}" == "true" ]
+then {
+    docker kill blog-app &&
+    docker rm blog-app &&
+    docker run --name blog-app --network=host -p 3000:3000 -d keyu/blog-app &&
+}
+else
+{
+    docker run --name blog-app --network=host -p 3000:3000 -d keyu/blog-app &&
+}
+fi
 echo 'OK'
