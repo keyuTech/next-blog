74b847c166f9173cbece4fe9dfc3f3eb6fe804fc
keyu <me@keyu.email>
2022-11-25 23:10:56 +0800

    feat: update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,7 +7,7 @@ yarn install --production=false &&
 npx prisma generate &&
 yarn build && 
 docker build -t keyu/blog-app . &&
-docker kill blog &&
-docker rm blog &&
+docker kill app &&
+docker rm app &&
 docker run --name app --network=host -p 3000:3000 -d keyu/blog-app &&
 echo 'OK'
