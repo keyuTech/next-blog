4e1e0651504df04441649d146cdead94db299774
keyu <me@keyu.email>
2022-11-26 15:40:36 +0800

    feat: add custom document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,11 +8,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   const { header } = useHeader();
   return (
     <div className={"h-screen w-screen overflow-hidden"}>
-      <Head>
-        <title>{`Keyu's Website`}</title>
-        <meta name="description" content="keyu's website" />
-        <link rel="shortcut icon" href="/public/seele-logo.svg" />
-      </Head>
       <div>
         {header}
         <div className={"mt-16"}>
---
+++ pages/_docuement.tsx
@@ -0,0 +1,35 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <title>{`Keyu's Website`}</title>
+          <meta name="description" content="keyu's website" />
+          <link rel="shortcut icon" href="../public/seele-logo.svg" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
